d292137e9a90fce538e82308ff912d89f423aaea
kbkarthickbalaji <karthickthug07@gmail.com>
2024-06-25 12:45:35 +0530

    Add files via upload

---
+++ pyt prg for gcd.py
@@ -0,0 +1,6 @@
+n1=int(input("enter a number1:"))
+n2=int(input("enter a number2:"))
+for i in range(i,n1+1):
+    if(n1%i==0 and n2%i==0):
+   gcd=i:
+    print(gcd)
---
+++ pyt prg for prime number.py
@@ -0,0 +1,11 @@
+lower = int(input("Enter a lower range: "))
+upper = int(input("Enter an upper range: "))
+
+for n in range(lower, upper + 1):
+    if n > 1:
+        
+        for i in range(2, n):
+            if (n % i) == 0:
+                break
+        else:
+            print(n)
