7bd5d3c8a4554d8e4dccc051250ef049a2968882
kbkarthickbalaji <karthickthug07@gmail.com>
2024-06-25 14:24:44 +0530

    Add files via upload

---
+++ pyt prg for palindrome.py
@@ -0,0 +1,10 @@
+n=eval(input("enter a number"))
+reverse=0
+while n > 0:
+a = n % 10
+reverse=(reverse*10)=+a
+n=n//10
+if n==reverse
+print("the given number is palindrome")
+else
+print("the given number is not a palindrome")
